4bf6f8c0e1e0a3463a8aea8d01dd8fd1232020fc
Nutan Shinde <nutanshinde028@gmail.com>
2026-08-17 14:04:16 +0530

    Add new LeetCode script

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let h2 = document.querySelector ("h2");
+// let h2 = document.querySelector ("h2");
 
-console.dir(h2.innerText);
+// console.dir(h2.innerText);
 
-h2.innerText = h2.innerText + " nutan college";
+// h2.innerText = h2.innerText + " nutan college from pune learn java";
 
 
 
+let divs = document.querySelectorAll(".box")
+// console.log(divs[0]);
+let idx = 1;
+for ( div of divs){
+    div.innerText =`new unique value${idx}`;
+    idx++;
+}
+// divs[0].innerText = " new unique value";
+// divs[1].innerText = " new unique value";
+// divs[2].innerText = " new unique value";
